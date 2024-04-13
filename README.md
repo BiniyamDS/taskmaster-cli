11f772144b4c38a32b4ef9508a50b4c55d599f6e
BiniyamDS <biniyam.yr@gmail.com>
2024-04-13 15:51:16 +0300

    added functionality to delete update and list todos

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # TaskMaster-CLI
 A simple todo list application for the CLI with the following options:
 - [x] --new to add a new todo item
-- [ ] --list [all|pending|done] to list the todo items
-- [ ] --done [id] to update a todo item
-- [ ] --delete [id] to delete a todo item
+- [x] --list [all|pending|done] to list the todo items
+- [x] --done [id] to update a todo item
+- [x] --delete [id] to delete a todo item
 - [ ] --help to list all the available options
 - [ ] --version to print the version of the application
+
+## Things to do
+- [ ] Ability to take in user input
--- app.js
+++ app.js
@@ -13,10 +13,11 @@ const { Client, Pool } = require("pg");
 const pool = new Pool(credentials);
 
 async function poolDemo() {
-	await createTodo('yet another', true);
-  const now = await pool.query("SELECT * from todos");
+  // await createTodo('yet another', true);
+  // await deleteTodo(5)
+  // await updateTodo(4, false)
+  await listTodos();
   await pool.end();
-  console.log(now.rows);
 }
 
 // function to create todo
@@ -24,11 +25,32 @@ async function poolDemo() {
 const createTodo = async (todo_text, done = false) => {
   const text = `insert into todos (text, done) values ($1, $2)`;
   const values = [todo_text, done];
-  const test = await pool.query(text, values);
+  await pool.query(text, values);
 };
 
 // function to delete todo
 
+const deleteTodo = async (id) => {
+  const text = `delete from todos where id=$1`;
+  const values = [id];
+  await pool.query(text, values);
+};
+
 // function to update todo
+
+const updateTodo = async (id, done) => {
+  const text = `update todos set done=$2 where id=$1`;
+  const values = [id, done];
+  await pool.query(text, values);
+};
+
+// function to list all todos
+const listTodos = async (all = true, done = false) => {
+  const base_text = `SELECT * from todos`;
+  const bool_text = `where done=${done}`;
+  const text = all ? base_text: `${base_text} ${bool_text}`;
+  const now = await pool.query(text);
+  console.log(now.rows);
+};
 
 poolDemo();
